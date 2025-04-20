3d24eb088b775180f6d3a0f9845f2f9171132f90
Andrew Petrochuk <petrochuk@msn.com>
2025-04-20 15:54:14 -0700

    Add RollingStandardDeviation class and unit tests

---
+++ src/AppCore.Tests/Math/RollingStandardDeviationTests.cs
@@ -0,0 +1,81 @@
+﻿using AppCore.Math;
+
+namespace AppCore.Tests.Math;
+
+[TestClass]
+public sealed class RollingStandardDeviationTests
+{
+    [TestMethod]
+    public void RollingStandardDeviation_Constructor_NullPeriod() {
+        // Arrange
+        var rollingStdDev = new RollingStandardDeviation(null);
+
+        // Act
+        rollingStdDev.AddValue(1.0);
+        rollingStdDev.AddValue(2.0);
+        Assert.AreEqual(0.70710678118654757, rollingStdDev.Value);
+
+        rollingStdDev.AddValue(3.0);
+        Assert.AreEqual(1, rollingStdDev.Value);
+
+        rollingStdDev.AddValue(4.0);
+        Assert.AreEqual(1.2909944487358056, rollingStdDev.Value);
+
+        rollingStdDev.AddValue(5.0);
+        Assert.AreEqual(1.5811388300841898, rollingStdDev.Value);
+
+        rollingStdDev.AddValue(6.0);
+        Assert.AreEqual(1.8708286933869707, rollingStdDev.Value);
+
+        rollingStdDev.AddValue(7.0);
+        Assert.AreEqual(2.1602468994692869, rollingStdDev.Value);
+
+        rollingStdDev.AddValue(8.0);
+        Assert.AreEqual(2.4494897427831779, rollingStdDev.Value);
+
+        rollingStdDev.AddValue(9.0);
+        Assert.AreEqual(2.7386127875258306, rollingStdDev.Value);
+
+        rollingStdDev.AddValue(10.0);
+        Assert.AreEqual(3.0276503540974917, rollingStdDev.Value);
+
+        // Assert
+        Assert.AreEqual(5.5, rollingStdDev.Mean);
+        Assert.AreEqual(10, rollingStdDev.Count);
+    }
+
+    [TestMethod]
+    public void RollingStandardDeviation_Constructor_Period() {
+        // Arrange
+        var rollingStdDev = new RollingStandardDeviation(4);
+
+        // Act
+        rollingStdDev.AddValue(1.0);
+        rollingStdDev.AddValue(2.0);
+        rollingStdDev.AddValue(3.0);
+        rollingStdDev.AddValue(4.0);
+        Assert.AreEqual(1.2909944487358056, rollingStdDev.Value);
+
+        rollingStdDev.AddValue(5.0);
+        Assert.AreEqual(1.6719966856027755, rollingStdDev.Value);
+
+        rollingStdDev.AddValue(6.0);
+        Assert.AreEqual(2.0669181588673822, rollingStdDev.Value);
+
+        rollingStdDev.AddValue(7.0);
+        Assert.AreEqual(2.4605485356453403, rollingStdDev.Value);
+
+        rollingStdDev.AddValue(8.0);
+        Assert.AreEqual(2.8469308881222566, rollingStdDev.Value);
+
+        rollingStdDev.AddValue(9.0);
+        Assert.AreEqual(3.2236280694147612, rollingStdDev.Value);
+
+        rollingStdDev.AddValue(10.0);
+        Assert.AreEqual(3.5897078903281687, rollingStdDev.Value);
+
+        // Assert
+        Assert.AreEqual(5.12880864739418, rollingStdDev.Mean);
+        Assert.AreEqual(4, rollingStdDev.Count);
+    }
+}
---
+++ src/AppCore/Math/RollingStandardDeviation.cs
@@ -0,0 +1,66 @@
+﻿namespace AppCore.Math;
+
+/// <summary>
+/// Rolling standard deviation calculator using Welford's method.
+/// Warning: For fixed periods, the algorithm can produce large errors when values trend becase the mean is changing.
+/// It works well for non-trending data with stable mean.
+/// </summary>
+public class RollingStandardDeviation
+{
+    private readonly int? _period;
+    private double _count;
+    private double _mean;
+    private double _sum;
+    private double _oldestValue;
+
+    public RollingStandardDeviation(int? period = null) {
+        if (period != null && period <= 1)
+            throw new ArgumentException($"Period must be greater than 1. Actual {period}", nameof(period));
+
+        _period = period;
+        _count = 0;
+        _mean = 0;
+        _sum = 0;
+        _oldestValue = 0;
+    }
+
+    public void AddValue(double value) {
+        if (!double.IsFinite(value))
+            return;
+
+        if (_period == null || _count < _period) {
+            // Add value to the rolling window
+            _count++;
+            double tempMean = _mean;
+            _mean += (value - tempMean) / _count;
+            _sum += (value - tempMean) * (value - _mean);
+        }
+        else {
+            // Remove the effect of the oldest value
+            double tempMean = _mean;
+            _mean -= (_oldestValue - tempMean) / _count;
+            _sum -= (_oldestValue - tempMean) * (_oldestValue - _mean);
+
+            // Add the new value
+            tempMean = _mean;
+            _mean += (value - tempMean) / _count;
+            _sum += (value - tempMean) * (value - _mean);
+        }
+
+        // Update the oldest value
+        _oldestValue = value;
+    }
+
+    public double Mean => _mean;
+
+    public double Value {
+        get {
+            if (_count < 2)
+                throw new InvalidOperationException("At least two values are required to calculate standard deviation.");
+
+            return System.Math.Sqrt(_sum / (_count - 1));
+        }
+    }
+
+    public int Count => (int)_count;
+}
